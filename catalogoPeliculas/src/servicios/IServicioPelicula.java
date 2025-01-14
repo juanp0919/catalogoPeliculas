c1ee9d867b192afb128b55412b5e929ec0ccc6f7
juanp0919 <100392226+juanp0919@users.noreply.github.com>
2025-01-14 18:54:05 -0500

    creacion de interface de serviciopelicula
    se crea los servicios de pelicula

---
+++ catalogoPeliculas/src/servicios/IServicioPelicula.java
@@ -0,0 +1,15 @@
+package servicios;
+
+import dominio.Pelicula;
+
+public interface IServicioPelicula {
+
+
+    public void listarPeliculas();
+
+    public void agregarPelicula(Pelicula pelicula);
+
+    public void buscarPelicula(Pelicula pelicula);
+
+
+}
